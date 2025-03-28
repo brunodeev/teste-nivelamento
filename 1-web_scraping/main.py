70802dcf5f095a55fc4c4ec9b5d706201afb65f5
Bruno César <bcgmeireles@gmail.com>
2025-03-28 20:00:08 -0300

    feat: implements beautifulsoup to print all links like 'Anexo'

--- 1-web_scraping/main.py
+++ 1-web_scraping/main.py
@@ -1,11 +1,19 @@
+import os
 import requests
+from bs4 import BeautifulSoup
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
 }
 
-url = "https://webcache.googleusercontent.com/search?q=cache:https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos"
+url = "https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos"
 
-r = requests.get(url, headers=headers)
+response = requests.get(url, headers=headers)
+soup = BeautifulSoup(response.text, 'html.parser')
 
-print(r.status_code)
+os.makedirs("downloads", exist_ok=True)
+pdfs = []
+
+for a in soup.find_all("a", href=True):
+    if not "(Anexo I" or "Anexo I" in a.text or "Anexo II"in a.text:
+        print(a.text)
