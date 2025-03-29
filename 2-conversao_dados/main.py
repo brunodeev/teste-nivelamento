c4134f2fd629fae1b27c6b36fb4f30d5688cadc2
Bruno <bcgmeireles@gmail.com>
2025-03-29 11:06:54 -0300

    feat: implements pdf download in test 2

---
+++ 2-conversao_dados/main.py
@@ -0,0 +1,10 @@
+import requests
+import os
+
+pdf_url = "https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos/Anexo_I_Rol_2021RN_465.2021_RN627L.2024.pdf"
+pdf_file = "anexo_i.pdf"
+
+if not os.path.exists(pdf_file):
+    r = requests.get(pdf_url)
+    with open(pdf_file, "wb") as f:
+        f.write(r.content)
