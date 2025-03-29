e7d024df2299e688683c578c2b5d1ca9e9f87af4
Bruno <bcgmeireles@gmail.com>
2025-03-29 11:26:11 -0300

    fix: try to use pdfplumber and panda to convert to csv and fix accentuation

--- 2-conversao_dados/main.py
+++ 2-conversao_dados/main.py
@@ -1,16 +1,29 @@
-import tabula
+import pdfplumber
+import pandas as pd
 import requests
-import os
 
 pdf_url = "https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos/Anexo_I_Rol_2021RN_465.2021_RN627L.2024.pdf"
 pdf_file = "anexo_i.pdf"
 
-if not os.path.exists(pdf_file):
-    r = requests.get(pdf_url)
-    with open(pdf_file, "wb") as f:
-        f.write(r.content)
+with open(pdf_file, "wb") as f:
+    f.write(requests.get(pdf_url).content)
 
-tabula.environment_info()
-dfs = tabula.read_pdf(pdf_file, encoding='latin1', pages="all", multiple_tables=True)
+dados = []
+with pdfplumber.open(pdf_file) as pdf:
+    for page in pdf.pages:
+        tables = page.extract_tables()
+        for table in tables:
+            if table and all(len(row) == len(table[0]) for row in table):
+                dados.extend(table)
 
-print(dfs)
+df = pd.DataFrame(dados)
+
+df = df[df[df.columns[0]] != df.columns[0]]
+
+df.replace({
+    "OD": "Consultas e outros procedimentos realizados por cirurgiões-dentistas",
+    "AMB": "Procedimentos realizados em ambiente ambulatorial"
+}, inplace=True)
+
+csv_file = "rol_procedimentos.csv"
+df.to_csv(csv_file, index=False, encoding="utf-8-sig")
