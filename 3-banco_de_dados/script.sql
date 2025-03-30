5d6343275b01ff8f64d639a0a6c8dc8fd3beece2
Bruno <bcgmeireles@gmail.com>
2025-03-30 11:21:51 -0300

    feat: implements filter select in 1t doc

--- 3-banco_de_dados/script.sql
+++ 3-banco_de_dados/script.sql
@@ -11,4 +11,12 @@ CREATE TABLE operadoras_ativas (
     VL_Saldo_Final FLOAT
 );
 
-SELECT * FROM operadoras_ativas;
+SELECT * FROM operadoras_ativas
+WHERE Descricao LIKE 'OUTROS DÉBITOS DE OPERAÇÕES COM PLANOS DE ASSISTÊNCIA MÉDICO HOSPITALAR'
+OR Descricao LIKE 'Despesas Financeiras com Operações de Assistência Médico-Hospitalar'
+OR Descricao LIKE 'DESPESAS FINANCEIRAS COM OPERAÇÕES DE ASSISTÊNCIA MÉDICO-HOSPITALAR';
+
+LOAD DATA INFILE '1T2023_formatado.csv' INTO TABLE operadoras_ativas
+FIELDS TERMINATED BY ';'
+IGNORE 1 LINES
+(Data, Reg_Ans, Cd_Conta_Contabil, Descricao, VL_Saldo, VL_Saldo_Final, @dummy);
