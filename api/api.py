1c426d31a8248f81e4018839d48dec0a01a4bfe7
Bruno César <bcgmeireles@gmail.com>
2025-04-01 08:37:47 -0300

    feat: implements file and read_csv

--- api/api.py
+++ api/api.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+import pandas as pd
 
 app = FastAPI()
 
-@app.get("/")
-def hello_world_root():
-    return {"Hello": "World"}
+df_operadoras = pd.read_csv("api/Relatorio_cadop.csv", sep=";", encoding="latin1")
+
+df_operadoras["Data_Registro_ANS"] = pd.to_datetime(df_operadoras["Data_Registro_ANS"], errors="coerce")
+
+@app.get("/operadoras")
+def operadoras():
+    return JSONResponse(content={operadoras})
